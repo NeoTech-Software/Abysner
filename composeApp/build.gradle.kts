bc9253271a553606da888695102547bef5ab4ad9
Rolf Smit <rolf@neotech.nl>
2024-08-23 10:08:13 +0200

    Release: 1.0.0-alpha02 (#19)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -22,9 +22,9 @@ import java.util.Properties
 
 // DMG distribution does not support "-alpha", MSI requires at least MAJOR.MINOR.BUILD
 val abysnerVersionBase = "1.0.0"
-val abysnerVersion = "$abysnerVersionBase-alpha"
+val abysnerVersion = "$abysnerVersionBase-alpha02"
 // iOS supports a String here, but Android only an integer
-val abysnerBuildNumber = 1
+val abysnerBuildNumber = 2
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
