118bb5b41f264626b96fcf516c62becb007bd81e
Rolf Smit <rolf@neotech.nl>
2026-09-11 14:51:27 +0200

    Fix: Android status bar icon colors in forced dark and light themes (#212)

--- composeApp/src/androidMain/kotlin/org/neotech/app/abysner/presentation/theme/Theme.android.kt
+++ composeApp/src/androidMain/kotlin/org/neotech/app/abysner/presentation/theme/Theme.android.kt
@@ -13,12 +13,16 @@
 package org.neotech.app.abysner.presentation.theme
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
 actual fun getColorScheme(dynamicColor: Boolean, isDarkMode: Boolean): ColorScheme = when {
@@ -33,14 +37,13 @@ actual fun getColorScheme(dynamicColor: Boolean, isDarkMode: Boolean): ColorSche
 @SuppressLint("ComposableNaming")
 @Composable
 actual fun applyPlatformSpecificThemeConfiguration(colorScheme: ColorScheme, isDarkMode: Boolean) {
-    // val view = LocalView.current
-    // if (!view.isInEditMode) {
-    //     SideEffect {
-    //         val window = (view.context as Activity).window
-    //         window.statusBarColor = colorScheme.primary.toArgb()
-    //         WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = isDarkMode
-    //     }
-    // }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDarkMode
+        }
+    }
 }
 
 @Suppress("SameReturnValue")
